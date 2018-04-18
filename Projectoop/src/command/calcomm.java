00043da033554ec6454b03824a40d17ac3f6abe3
Wipanan6650 <RAPHEE@172.19.192.185>
2018-04-18 19:49:15 +0700

    calcomm

--- Projectoop/src/command/calcomm.java
+++ Projectoop/src/command/calcomm.java
@@ -4,13 +4,17 @@ import java.sql.*;
 
 abstract public class calcomm extends saler implements connectDB {
     public void calcom(){
-        String sql;
-        float sale;
+        String sql="select s.orderNO,s.ProID,p.price,s.ID from sale as s left join product as p on s.ProID=p.ProID where s.ID='"+super.getID()+"';";
+        float sale=(float) 0.00;
         try{
             Connection con = getconnection();
             Statement stm=con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             ResultSet rs=stm.executeQuery(sql);
             rs.first();
+            do{
+                sale=sale+rs.getFloat(3);
+                rs.next();
+            }while(rs.last());
         }
         catch(Exception e){
             e.printStackTrace();
